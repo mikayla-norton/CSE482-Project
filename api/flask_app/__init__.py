ede7acb3cfab1a1bf2b48f817799de766dd54d04
msusaes <norton18@msu.edu>
2023-12-11 16:03:55 -0500

    testing reduced requirements

--- api/flask_app/__init__.py
+++ api/flask_app/__init__.py
@@ -1,12 +1,12 @@
 import os
 from flask import Flask
-from flask_failsafe import failsafe
-from flask_cors import CORS
+#from flask_failsafe import failsafe
+#from flask_cors import CORS
 
-@failsafe
+#@failsafe
 def create_app():
     app = Flask(__name__)
-    CORS(app)
+    #CORS(app)
     with app.app_context():
         from . import routes
         return app
